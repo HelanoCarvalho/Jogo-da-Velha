8dfb732b54862470599765b8ef33136e438216e9
HelanoCarvalho <helanonucleo@gmail.com>
2021-04-21 13:53:12 -0300

    idk

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 var vez = 1
 var vencedor = '' 
+var preenchido = 0
 
 var divjogo = document.getElementById('jogo')
-
-var div = document.getElementById('quadrado1')
-var div2 = document.getElementById('quadrado2')
-var div3 = document.getElementById('quadrado3')
-var div4 = document.getElementById('quadrado4')
-var div5 = document.getElementById('quadrado5')
-var div6 = document.getElementById('quadrado6')
-var div7 = document.getElementById('quadrado7')
-var div8 = document.getElementById('quadrado8')
-var div9 = document.getElementById('quadrado9')
 
 var um = ''
 var dois = ''
@@ -23,113 +14,123 @@ var sete = ''
 var oito = ''
 var nove = ''
 
+var div = document.getElementsByClassName('quadrado')
 
 function jogar1(){
     vez += 1
-    if( div.click && vez % 2 == 0){
-        div.style.backgroundImage = "url(X.png)"
+    if( div[0].click && vez % 2 == 0){
+        div[0].style.backgroundImage = "url(X.png)"
         um = 'X'
-    }else if( div.click && vez % 2 == 1){
-        div.style.backgroundImage = "url(O.png)"
+    }else if( div[0].click && vez % 2 == 1){
+        div[0].style.backgroundImage = "url(O.png)"
         um = 'O'
     }
-    div.removeAttribute('onclick')
+    preenchido += 1
+    div[0].removeAttribute('onclick')
 }
 
 function jogar2(){
     vez += 1
-    if( div2.click && vez % 2 == 0){
-        div2.style.backgroundImage = "url(X.png)"
+    if( div[1].click && vez % 2 == 0){
+        div[1].style.backgroundImage = "url(X.png)"
         dois = 'X'
-    }else if( div2.click && vez % 2 == 1){
-        div2.style.backgroundImage = "url(O.png)"
+    }else if( div[1].click && vez % 2 == 1){
+        div[1].style.backgroundImage = "url(O.png)"
         dois = 'O'
     }
-    div2.removeAttribute('onclick')
+    preenchido += 1
+    div[1].removeAttribute('onclick')
 }
 
 function jogar3(){
     vez += 1
-    if( div3.click && vez % 2 == 0){
-        div3.style.backgroundImage = "url(X.png)"
+    if( div[2].click && vez % 2 == 0){
+        div[2].style.backgroundImage = "url(X.png)"
         tres = 'X'
-    }else if( div3.click && vez % 2 == 1){
-        div3.style.backgroundImage = "url(O.png)"
+    }else if( div[2].click && vez % 2 == 1){
+        div[2].style.backgroundImage = "url(O.png)"
         tres = 'O'
     }
-    div3.removeAttribute('onclick')
+    preenchido += 1
+    div[2].removeAttribute('onclick')
 }
 
 function jogar4(){
     vez += 1
-    if( div4.click && vez % 2 == 0){
-        div4.style.backgroundImage = "url(X.png)"
+    if( div[3].click && vez % 2 == 0){
+        div[3].style.backgroundImage = "url(X.png)"
         quatro = 'X'
-    }else if( div4.click && vez % 2 == 1){
-        div4.style.backgroundImage = "url(O.png)"
+    }else if( div[3].click && vez % 2 == 1){
+        div[3].style.backgroundImage = "url(O.png)"
         quatro = 'O'
     }
-    div4.removeAttribute('onclick')
+    preenchido += 1
+    div[3].removeAttribute('onclick')
 }
 
 function jogar5(){
     vez += 1
-    if( div5.click && vez % 2 == 0){
-        div5.style.backgroundImage = "url(X.png)"
+    if( div[4].click && vez % 2 == 0){
+        div[4].style.backgroundImage = "url(X.png)"
         cinco = 'X'
-    }else if( div5.click && vez % 2 == 1){
-        div5.style.backgroundImage = "url(O.png)"
+    }else if( div[4].click && vez % 2 == 1){
+        div[4].style.backgroundImage = "url(O.png)"
         cinco = 'O'
     }
-    div5.removeAttribute('onclick')
+    preenchido += 1
+    div[4].removeAttribute('onclick')
 }
 
 function jogar6(){
     vez += 1
-    if( div6.click && vez % 2 == 0){
-        div6.style.backgroundImage = "url(X.png)"
+    if( div[5].click && vez % 2 == 0){
+        div[5].style.backgroundImage = "url(X.png)"
         seis = 'X'
-    }else if( div6.click && vez % 2 == 1){
-        div6.style.backgroundImage = "url(O.png)"
+    }else if( div[5].click && vez % 2 == 1){
+        div[5].style.backgroundImage = "url(O.png)"
         seis = 'O'
     }
-    div6.removeAttribute('onclick')
+    preenchido += 1
+    div[5].removeAttribute('onclick')
 }
 
 function jogar7(){
     vez += 1
-    if( div7.click && vez % 2 == 0){
-        div7.style.backgroundImage = "url(X.png)"
+    if( div[6].click && vez % 2 == 0){
+        div[6].style.backgroundImage = "url(X.png)"
         sete = 'X'
-    }else if( div7.click && vez % 2 == 1){
-        div7.style.backgroundImage = "url(O.png)"
+    }else if( div[6].click && vez % 2 == 1){
+        div[6].style.backgroundImage = "url(O.png)"
         sete = 'O'
     }
-    div7.removeAttribute('onclick')
+    preenchido += 1
+    div[6].removeAttribute('onclick')
 }
 
 function jogar8(){
     vez += 1
-    if( div8.click && vez % 2 == 0){
-        div8.style.backgroundImage = "url(X.png)"
+    if( div[7].click && vez % 2 == 0){
+        div[7].style.backgroundImage = "url(X.png)"
         oito = 'X'
-    }else if( div8.click && vez % 2 == 1){
-        div8.style.backgroundImage = "url(O.png)"
+    }else if( div[7].click && vez % 2 == 1){
+        div[7].style.backgroundImage = "url(O.png)"
         oito = 'O'
     }
-    div8.removeAttribute('onclick')
+    preenchido += 1
+    div[7].removeAttribute('onclick')
 }
 
 function jogar9(){
     vez += 1
-    if( div9.click && vez % 2 == 0){
-        div9.style.backgroundImage = "url(X.png)"
+    if( div[8].click && vez % 2 == 0){
+        div[8].style.backgroundImage = "url(X.png)"
         nove = 'X'
-    }else if( div9.click && vez % 2 == 1){
-        div9.style.backgroundImage = "url(O.png)"
+    }else if( div[8].click && vez % 2 == 1){
+        div[8].style.backgroundImage = "url(O.png)"
         nove = 'O'
     }
-    div9.removeAttribute('onclick')
+    preenchido += 1
+    div[8].removeAttribute('onclick')
 }
 
 function teste(a, b, c){
@@ -137,7 +138,9 @@ function teste(a, b, c){
         vencedor = 'O'
    }else if(a == 'X' && b == 'X' && c == 'X'){
         vencedor = 'X'
-}
+   }else if (preenchido == 9 && vencedor != 'O' && vencedor != 'X'){
+       vencedor = 'Velha'
+   }
 }
 
 function fim(){
@@ -150,32 +153,24 @@ function fim(){
     teste(tres, cinco, sete) // X
     teste(um, cinco, nove) // X
 
+    if(vencedor != ''){
+    var fim =  document.createElement('div')
+    var botao = document.createElement('input')
+    var x = document.createElement('h2')
+    botao.setAttribute('type', 'button')
+    botao.setAttribute('value', 'Jogar Novamente')
+    botao.setAttribute('onclick', 'window.location.reload()')
+    fim.appendChild(x)
+    fim.appendChild(botao)
+    fim.setAttribute('id', 'fim')
+    divjogo.appendChild(fim)
+}
     if(vencedor == 'X'){
-        var fim =  document.createElement('div')
-        var botao = document.createElement('input')
-        var x = document.createElement('h2')
-        x.innerHTML = 'Jogador X ganhou'
-        botao.setAttribute('type', 'button')
-        botao.setAttribute('value', 'Jogar Novamente')
-        botao.setAttribute('onclick', 'window.location.reload()')
-        fim.appendChild(x)
-        fim.appendChild(botao)
-        fim.setAttribute('id', 'fim')
-        divjogo.appendChild(fim)
-       
+    x.innerHTML = 'Jogador X ganhou'  
     }else if(vencedor == 'O'){
-        var fim =  document.createElement('div')
-        var botao = document.createElement('input')
-        var x = document.createElement('h2')
-        x.innerHTML = 'Jogador O ganhou'
-        botao.setAttribute('type', 'button')
-        botao.setAttribute('value', 'Jogar Novamente')
-        botao.setAttribute('onclick', 'window.location.reload()')
-        fim.appendChild(x)
-        fim.appendChild(botao)
-        fim.setAttribute('id', 'fim')
-        divjogo.appendChild(fim)
-       
+        x.innerHTML = 'Jogador O ganhou'     
+    }else if(vencedor == 'Velha'){   
+        x.innerHTML = 'Deu Velha'
     }
 
 }
